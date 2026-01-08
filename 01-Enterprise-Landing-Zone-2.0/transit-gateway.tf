a5cbdfaf54bf77efdd566a78990072695437ee67
abhiibansal <abhinavbansal2018@gmail.com>
2026-01-08 23:41:20 +0530

    feat: share transit gateway with workloads ou

--- 01-Enterprise-Landing-Zone-2.0/transit-gateway.tf
+++ 01-Enterprise-Landing-Zone-2.0/transit-gateway.tf
@@ -37,3 +37,17 @@ module "transit_gateway" {
     Role        = "Network-Hub"
   }
 }
+
+# -----------------------------------------------------------------------
+# Phase 3 Extension: Enterprise Sharing
+# -----------------------------------------------------------------------
+# This invites the entire "Workloads" Organizational Unit (Dev & Prod)
+# to use our Transit Gateway. AWS handles the permissions automatically.
+
+resource "aws_ram_principal_association" "workloads_ou" {
+  # The Ticket: We reference the RAM Share ID created by the module above
+  resource_share_arn = module.transit_gateway.ram_resource_share_id
+
+  # The Recipient: The ARN of your Workloads OU
+  principal = "arn:aws:organizations::311472845767:ou/o-lfpq3a1r1l/ou-wkdq-d1fq0ffk"
+}
